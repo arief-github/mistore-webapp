bb90161868213b501071d9860412d8a88d156c36
Arief Kurniawan <78076941+arief-github@users.noreply.github.com>
2024-02-14 23:16:23 +0700

    feat: add retrieve customers data feature

--- components/admin/sidebar.vue
+++ components/admin/sidebar.vue
@@ -4,7 +4,7 @@
       <nuxt-link :to="{name: 'admin-dashboard'}" class="c-sidebar-nav-link" href="index.html">
         <svg class="c-sidebar-nav-icon">
           <use
-            xlink:href="@/node_modules/@coreui/icons/sprites/free.svg#cilspeedometer"></use>
+            xlink:href="@/node_modules/@coreui/icons/sprites/free.svg#cil-speedometer"></use>
         </svg> Dashboard
       </nuxt-link>
     </li>
@@ -13,7 +13,7 @@
       <svg
         class="c-sidebar-nav-icon">
         <use
-          xlink:href="@/node_modules/@coreui/icons/sprites/free.svg#cilfolder"></use>
+          xlink:href="@/node_modules/@coreui/icons/sprites/free.svg#cil-folder"></use>
       </svg>
       Categories</nuxt-link>
     </li>
@@ -21,7 +21,7 @@
       <svg
         class="c-sidebar-nav-icon">
         <use
-          xlink:href="@/node_modules/@coreui/icons/sprites/free.svg#cillayers"></use>
+          xlink:href="@/node_modules/@coreui/icons/sprites/free.svg#cil-layers"></use>
       </svg>
       Products</nuxt-link>
     </li>
@@ -30,23 +30,23 @@
       <svg
         class="c-sidebar-nav-icon">
         <use
-          xlink:href="@/node_modules/@coreui/icons/sprites/free.svg#cilcart"></use>
+          xlink:href="@/node_modules/@coreui/icons/sprites/free.svg#cil-cart"></use>
       </svg>
       Invoices</a>
     </li>
-    <li class="c-sidebar-nav-item"><a href="#" class="c-sidebar-nav-link">
+    <li class="c-sidebar-nav-item"><nuxt-link :to="{name: 'admin-customers'}" class="c-sidebar-nav-link">
       <svg
         class="c-sidebar-nav-icon">
         <use
-          xlink:href="@/node_modules/@coreui/icons/sprites/free.svg#ciluser"></use>
+          xlink:href="@/node_modules/@coreui/icons/sprites/free.svg#cil-user"></use>
       </svg>
-      Customers</a>
+      Customers</nuxt-link>
     </li>
     <li class="c-sidebar-nav-item"><nuxt-link :to="{name: 'admin-sliders'}" class="c-sidebar-nav-link">
       <svg
         class="c-sidebar-nav-icon">
         <use
-          xlink:href="@/node_modules/@coreui/icons/sprites/free.svg#cillaptop"></use>
+          xlink:href="@/node_modules/@coreui/icons/sprites/free.svg#cil-laptop"></use>
       </svg>
       Sliders</nuxt-link>
     </li>
@@ -54,7 +54,7 @@
       <svg
         class="c-sidebar-nav-icon">
         <use
-          xlink:href="@/node_modules/@coreui/icons/sprites/free.svg#cilgroup"></use>
+          xlink:href="@/node_modules/@coreui/icons/sprites/free.svg#cil-group"></use>
       </svg>
       Users</nuxt-link>
     </li>
